c5cc1f3793b96bde601cb268879b580b20ba7493
Dhairya Kubadia <dkk080504@gmail.com>
2025-12-28 21:15:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,11 +19,16 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: 587,
   secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
+
 
 /* ===== Lead API ===== */
 app.post("/api/lead", async (req, res) => {
